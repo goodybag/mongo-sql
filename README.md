bc4c54eeaa5f3190cdad66d322925267891e66ac
John Farkett <john@pepper.(none)>
2013-04-02 21:57:04 -0500

    break tests out into own files, npmifiy, readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# MongoPG
+
+A mongo-like interface for node postgres.
+
+```javascript
+// Lookup user by id
+db.users.findOne(userId, function(error, result){
+/* ... */
+});
+
+// Little more complex
+db.users.find(
+  {
+    $notNull: ['email']
+  , $leftJoin: { groups: { 'groups.userId': 'users.id'  }
+  }
+, { 
+    fields: ['users.*', 'array_agg(groups.name) as groups']
+  , offset: 25
+  , limit:  50
+  , order: 'id desc'
+  }
+, function(error, results){
+    /* .. */
+  }
+);
+```
--- test/test.js
+++ test/test.js
@@ -1,46 +1,8 @@
 var
-  assert        = require("assert")
+  assert        = require('assert')
+, utils         = require('../lib/utils')
 , QueryBuilder  = require('../lib/query-builder')
-
 , collection    = new QueryBuilder('collection')
 ;
 
-describe('Query Builder', function(){
-  describe('collection.find', function(){
-
-    it('{ id: 5 }', function(){
-      assert.equal(
-        collection.find({ id: 5 })
-      , 'select "collection".* from "collection" where ("id" = 5)'
-      );
-    });
-
-    it('{ $gt: { id: 5, other: 10 } }', function(){
-      assert.equal(
-        collection.find({ $gt: { id: 5, other: 10 } })
-      , 'select "collection".* from "collection" where (("id" > 5 and "other" > 10))'
-      );
-    });
-
-    it('{ $or: [{ a: 5, b: 6 }, { c: 7, d: 8 }] }', function(){
-      assert.equal(
-        collection.find({ $or: [{ a: 5, b: 6 }, { c: 7, d: 8 }] })
-      , 'select "collection".* from "collection" where ((("a" = 5 and "b" = 6) or ("c" = 7 and "d" = 8)))'
-      );
-    });
-
-    it('{ $or: [{ a: 5, b: 6 }, { c: 7, d: 8 }] }', function(){
-      assert.equal(
-        collection.find({ $or: [{ a: { $gt: 7 } }, { c: 7, d: 8 }] })
-      , 'select "collection".* from "collection" where (((("a" > 7)) or ("c" = 7 and "d" = 8)))'
-      );
-    });
-
-    it('{ $or: [{ a: 5, b: 6 }, { c: 7, d: 8 }] }', function(){
-      assert.equal(
-        collection.find({ id: { $in: collection.find({ id: { $gt: 5 } }, { fields: ['id'] }) } })
-      , 'select "collection".* from "collection" where (("id" in (select id from "collection" where (("id" > 5)))))'
-      );
-    });
-  });
-});
+utils.requireDir('./commands');
