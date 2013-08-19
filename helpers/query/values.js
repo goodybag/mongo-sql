dd4d549226f5d71d5734d32716a3241130c4f42e
John Fawcett <jrf0110@gmail.com>
2013-08-19 14:41:15 -0500

    added expression to insert, columns adjusted to work for insert, allow sub-query field values, and expressions in place of values

--- helpers/query/values.js
+++ helpers/query/values.js
@@ -6,6 +6,7 @@ if (typeof module === 'object' && typeof define !== 'function') {
 
 define(function(require, exports, module){
   var helpers = require('../../lib/query-helpers');
+  var queryBuilder = require('../../lib/query-builder');
 
   helpers.register('values', function(values, valuesArray, query){
     if (typeof values != 'object') throw new Error('Invalid values input in query properties')
@@ -15,8 +16,13 @@ define(function(require, exports, module){
 
     var output = '("' + Object.keys(values).join('", "') + '") values (';
 
-    for (var key in values)
-      output += '$' + valuesArray.push(values[key]) + ', ';
+    for (var key in values){
+      if (typeof values[ key ] == 'object' && 'type' in values[ key ]){
+        output += '(' + queryBuilder( values[ key ], valuesArray ) + '), ';
+      } else {
+        output += '$' + valuesArray.push(values[key]) + ', ';
+      }
+    }
 
     if (output.length > 0) output = output.substring(0, output.length - 2);
 
