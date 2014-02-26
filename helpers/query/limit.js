aa536c7297c6bf1847535829f71a22b20aba689c
Preston Pham <prestonp08@gmail.com>
2014-02-26 14:23:53 -0600

    handle uppercase too

--- helpers/query/limit.js
+++ helpers/query/limit.js
@@ -8,9 +8,9 @@ define(function(require, exports, module){
   var helpers = require('../../lib/query-helpers');
 
   helpers.register('limit', function(limit, values){
-    if ( typeof limit === "number" )
+    if ( typeof limit === 'number' )
       return " limit $" + values.push(limit);
-    else if ( limit === "all" )
+    else if ( typeof limit === 'string' && limit.toLowerCase() === "all" )
       return " limit all";
     else
       throw new Error('Invalid limit type `' + typeof limit  + '` for query helper `limit`. Limit must be number or \'all\'');
